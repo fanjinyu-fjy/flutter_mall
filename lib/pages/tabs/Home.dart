9b6467956f0139bebe7cda8a184cdbe5fbd5b4eb
huanyueyaoqin <huanyueyaoqin@qq.com>
2020-03-07 00:31:54 +0800

    HomePage布局 终端适配

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,20 +10,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // 轮播图
+  Widget _swiperWidget() {
+    List<Map> imgList = [
+      {"url": "https://www.itying.com/images/flutter/slide01.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide02.jpg"},
+      {"url": "https://www.itying.com/images/flutter/slide03.jpg"},
+    ];
+    return Container(
+        child: AspectRatio(
+            aspectRatio: 2 / 1,
+            child: Swiper(
+                itemBuilder: (BuildContext context, int index) {
+                  return new Image.network(imgList[index]['url'],
+                      fit: BoxFit.fill);
+                },
+                itemCount: imgList.length,
+                pagination: new SwiperPagination(),
+                autoplay: true)));
+  }
+
+  Widget _titleWidget(value) {
+    return Container(
+      height: ScreenUtil.getInstance().setHeight(32),
+      margin: EdgeInsets.only(left: ScreenUtil.getInstance().setWidth(20)),
+      padding: EdgeInsets.only(left: ScreenUtil.getInstance().setWidth(20)),
+      decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+        color: Colors.red,
+        width: ScreenUtil.getInstance().setWidth(10),
+      ))),
+      child: Text(value, style: TextStyle(color: Colors.black54)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-            RaisedButton(
-              child: Text('跳转到搜索'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/search');
-              },
-            )
-          ])),
+    ScreenUtil.instance = ScreenUtil(width: 750, height: 1334)..init(context);
+
+    return ListView(
+      children: <Widget>[
+        _swiperWidget(),
+        SizedBox(height: 10),
+        _titleWidget('猜你喜欢'),
+        SizedBox(height: 10),
+        _titleWidget('全部商品'),
+      ],
     );
   }
 }
